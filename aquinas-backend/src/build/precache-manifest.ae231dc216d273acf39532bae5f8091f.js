5dd12d6b92226ac59ca61c36a39706cf5af5d761
Blake <blake.cs.ml@gmail.com>
2020-11-06 09:58:46 -0500

    updates for build

---
+++ aquinas-backend/src/build/precache-manifest.ae231dc216d273acf39532bae5f8091f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4242bf0884dce468c373f54f85be17c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a59a87086c380eaf246b",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "8d962351b4a35a802c02",
+    "url": "/static/css/main.7874f7c6.chunk.css"
+  },
+  {
+    "revision": "a59a87086c380eaf246b",
+    "url": "/static/js/2.b0bdc76b.chunk.js"
+  },
+  {
+    "revision": "825b3f1924aa91c0b22224e02cce27f9",
+    "url": "/static/js/2.b0bdc76b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d962351b4a35a802c02",
+    "url": "/static/js/main.7dde6b7b.chunk.js"
+  },
+  {
+    "revision": "7054d90776a57f20b3a5",
+    "url": "/static/js/runtime-main.72d630f4.js"
+  },
+  {
+    "revision": "c77578a21f134660fd7fae887d9854e3",
+    "url": "/static/media/logo.c77578a2.png"
+  }
+]);
